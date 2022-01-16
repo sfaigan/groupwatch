cae0220e4bc54a936a03f908042c43c344c1dddc
ACHultman <38937780+ACHultman@users.noreply.github.com>
2022-01-15 16:20:59 -0800

    node cache types

--- server/src/cache.ts
+++ server/src/cache.ts
@@ -1,4 +1,4 @@
-const NodeCache = require("node-cache");
+import NodeCache from "node-cache";
 
 export const roomCache = new NodeCache({
   stdTTL: 7200, // 2 days
@@ -7,7 +7,28 @@ export const roomCache = new NodeCache({
 // Model: Room
 // {
 //   [roomCode: string]: {
-//       currentPage: number,
-//       ...
+//       groupSize: number,
+//       users: {
+//          [userId: string]: {
+//              page: number,
+//          },
+//       },
+//       movies: {
+//          [movieId: number]: {
+//              yesCount: number,
+//              noCount: number,
+//              maybeCount: number,
+//              yesVotes: [userIds],
+//              noVotes: [userIds],
+//              maybeVotes: [userIds],
+//          },
+//       },
 //    }
 // }
+
+// roomCache.set("RMCODE", {})
+// roomCache.get("RMCODE").users["userId"] = {page: 1} -> adding a user, by reference (i think)
+// roomCache.get(roomCode).groupSize -> use in SS movie match logic
+// roomCache.get(roomCode).movies[movieId].yesCount -> use in SS movie match logic
+// roomCache.get(roomCode).movies[movieId].maybeCount -> use in SS movie match logic
+// roomCache.get(roomCode).users[userId].currentPage -> use in movie query
