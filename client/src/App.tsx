ac39578b825732729a6c19cd63d877bfbb29551c
Collin Eng <eng.engineereng@gmail.com>
2022-01-15 16:31:47 -0800

    Implement landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import * as React from "react"
+import React, { SyntheticEvent, useState } from "react"
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 import {
   ChakraProvider,
   Box,
@@ -6,33 +7,29 @@ import {
   Link,
   VStack,
   Code,
+  Input,
   Grid,
   theme,
+  Button,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import { LandingPage } from "./views/LandingPage";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+const breakpoints = createBreakpoints({
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+})
+
+export const App = () => { 
+  
+    return (<LandingPage/>)
+  // const [view, setView] = useState(0);
+  // var render;
+  // switch (view) {
+  //   case 0: render=<LandingPage/>
+  // }
+  // return (render)
+}
