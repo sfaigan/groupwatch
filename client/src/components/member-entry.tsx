6d362c4dd0b88ae9fba8d35bb6bd709b5daf8f31
Adam Hultman <38937780+ACHultman@users.noreply.github.com>
2022-01-16 07:54:57 -0800

    fix dark theme readability, tweak margins of user list

--- client/src/components/member-entry.tsx
+++ client/src/components/member-entry.tsx
@@ -1,46 +1,48 @@
-import {
-    Badge,
-    Button,
-  } from "@chakra-ui/react"
-import { BadgeProps, BadgeType } from "../constants"
+import { Badge, Button } from "@chakra-ui/react";
+import { BadgeProps, BadgeType } from "../constants";
 export interface Props {
-    text: string;
-    //callback: any; //TODO allow parent to see whether this is selected
-    type: BadgeType;
+  text: string;
+  //callback: any; //TODO allow parent to see whether this is selected
+  type: BadgeType;
 }
 
 export const MemberEntry = (props: Props) => {
-    var badge;
-    var label;
-    var color;
+  var badge;
+  var label;
+  var color;
 
-    if (props.type !== BadgeType.NOBADGE) {
-        switch (props.type) {
-            case BadgeType.NO:
-                color = BadgeProps.NO.colour;
-                label = BadgeProps.NO.label;
-                break;
-            case BadgeType.MAYBE:
-                color = BadgeProps.MAYBE.colour;
-                label = BadgeProps.MAYBE.label;
-                break;
-            case BadgeType.YES:
-                color = BadgeProps.YES.colour;
-                label = BadgeProps.YES.label;
-                break;
-            case BadgeType.READY:
-                color = BadgeProps.READY.colour;
-                label = BadgeProps.READY.label;
-            break;
-        }
-        badge = <Badge colorScheme={color}>{label}</Badge>
-
+  if (props.type !== BadgeType.NOBADGE) {
+    switch (props.type) {
+      case BadgeType.NO:
+        color = BadgeProps.NO.colour;
+        label = BadgeProps.NO.label;
+        break;
+      case BadgeType.MAYBE:
+        color = BadgeProps.MAYBE.colour;
+        label = BadgeProps.MAYBE.label;
+        break;
+      case BadgeType.YES:
+        color = BadgeProps.YES.colour;
+        label = BadgeProps.YES.label;
+        break;
+      case BadgeType.READY:
+        color = BadgeProps.READY.colour;
+        label = BadgeProps.READY.label;
+        break;
     }
+    badge = <Badge colorScheme={color}>{label}</Badge>;
+  }
 
-    return (
-        <Button justifyContent='space-between' backgroundColor='gray.100' variant='outline' width='100%'>
-            {props.text}
-            {badge}
-        </Button>
-    )
-}
+  return (
+    <Button
+      justifyContent="space-between"
+      color="current"
+      variant="outline"
+      width="100%"
+      marginBottom={2}
+    >
+      {props.text}
+      {badge}
+    </Button>
+  );
+};
